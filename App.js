d1d769bd91f7b472950565007fccc86fa86d68e5
Jonatan Nyberg <33462941+nybbe123@users.noreply.github.com>
2023-03-08 14:15:06 +0100

    basic components

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import {
+  StyleSheet, // For CSS
+  Text, // For headings and bread text
+  TextInput, // For inputfields
+  View, // Like an Div
+  Button, // non modable button
+  ScrollView, // To put lots of items that you can scroll through (not good for large amount of items - speed)
+  FlatList, // like ScrollView but doesnt render all items at once - only the nearest your current view
+} from 'react-native'
 
 export default function App() {
+  const [inputValue, setInputValue] = useState('')
+  const [goals, setGoals] = useState([])
+
+  function handleSubmit() {
+    setGoals((prevValue) => [
+      ...prevValue,
+      { text: inputValue, key: Math.random().toString },
+    ])
+    setInputValue('')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={s.container}>
+      <View style={s.inputContainer}>
+        <TextInput
+          style={s.textInput}
+          placeholder="CouseGoal"
+          value={inputValue}
+          onChangeText={(text) => setInputValue(text)}
+        />
+        <Button title="Add Goal" onPress={handleSubmit} />
+      </View>
+      <View style={s.goalContainer}>
+        {/* <ScrollView> */}
+        <FlatList
+          data={goals}
+          renderItem={(goalData) => {
+            return (
+              <View style={s.goalItem}>
+                <Text style={s.goalItemText}>{goalData.item.text}</Text>
+              </View>
+            )
+          }}
+        />
+        {/* </ScrollView> */}
+      </View>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
+const s = StyleSheet.create({
   container: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 25,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    flex: 1,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '80%',
+    padding: 8,
+  },
+  goalContainer: {
+    flex: 5,
+    marginTop: 25,
+  },
+  goalItem: {
+    margin: 8,
+    borderRadius: 8,
+    backgroundColor: '#5e0acc',
+    padding: 8,
+  },
+  goalItemText: {
+    color: '#fafafa',
+  },
+})
