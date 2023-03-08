7ca0d814588f18725419715f15517745df605b00
Jonatan Nyberg <33462941+nybbe123@users.noreply.github.com>
2023-03-08 18:31:52 +0100

    setup and routing

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StyleSheet, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from './screens/HomeScreen'
+import NewGameScreen from './screens/NewGameScreen'
 
 export default function App() {
-  return <View style={s.container}></View>
+  const Stack = createNativeStackNavigator()
+  return (
+    <NavigationContainer theme={{ colors: { background: '#ffffff' } }}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: 'Home',
+            headerStyle: {
+              backgroundColor: 'orange',
+            },
+            headerTintColor: 'black',
+            headerTitleAlign: 'center',
+          }}
+        />
+        <Stack.Screen
+          name="NewGame"
+          component={NewGameScreen}
+          options={{
+            title: 'New Game',
+            headerStyle: {
+              backgroundColor: 'orange',
+            },
+            headerTintColor: 'black',
+            headerTitleAlign: 'center',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const s = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-})
